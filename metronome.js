f9398fc3d16c2886787ef3d279b02ef2b559184f
Andrey Temnikov <andreytemn@mail.ru>
2017-07-04 23:41:54 +0500

    Migrated to Object-oriented tracklist

--- metronome.js
+++ metronome.js
@@ -1,21 +1,53 @@
 window.onload = init;
+var trackArray = [];
+var currentTrack;
 
-function init() {
-	setTrackData;	
-	document.getElementById("addNewTrack").onclick = addTrack;
-	document.getElementById("fraction1").onchange = setTrackData;
-	document.getElementById("fraction2").onchange = setTrackData;
-	document.getElementById("tempo").onchange = setTrackData;
+function Track(fraction1, fraction2, tempo, count, line) {
+	this.fraction1 = fraction1;
+	this.fraction2 = fraction2;
+	this.tempo = tempo;
+	this.count = count;
+	this.line = line;
 	
+	this.setLineValue = function(){
+		var thisLine = line.getElementsByTagName("trackDescriptor");
+		thisLine.value = this.fraction1 + "/" + this.fraction2 + "; " + this.tempo;
+	}
 }
 
-function setTrackData() {
-	var lines = document.getElementsByName("trackDescriptor");
-	var currentLine = lines[lines.length - 1];
+function getTrackById(id)
+{
+	for (var track in trackArray)
+	{
+		if (track.line == id)
+		{
+			return track;
+		}
+	}
+	return null;
+}
+
+
+function init() {
+	addTrack();
+	document.getElementById("addNewTrack").onclick = addTrack;
+	document.getElementById("fraction1").onchange = function(){
+		currentLine.fr1 = document.getElementById("fraction1").value;
+		currentLine.setLineValue();
+	};
+	document.getElementById("fraction2").onchange = function(){
+		currentLine.fr2 = document.getElementById("fraction2").value;
+		currentLine.setLineValue();
+	};
+	document.getElementById("tempo").onchange = function(){
+		currentLine.tempo = document.getElementById("tempo").value;
+		currentLine.setLineValue();
+	};
+}
+
+function onFr1Changed()
+{
 	var fr1 = document.getElementById("fraction1").value;
-	var fr2 = document.getElementById("fraction2").value;
-	var tmp = document.getElementById("tempo").value;
-	currentLine.value = fr1 + "/" + fr2 + "; " + tmp;
 }
 
 function switchLamp() {
@@ -27,8 +59,29 @@ function switchLamp() {
 }
 
 function addTrack() {
+	
 	var trackList = document.getElementById("trackList");
-	var newTrackLi = document.createElement("li");
+	var newLine = createTrackLine();
+	newLine.onclick = function (newLine)
+	{
+		alert(newLine);
+		newLine.style.border = "1px solid black";
+		currentLine = track;
+	};
+	trackList.appendChild(newLine);
+	
+	var fr1 = document.getElementById("fraction1").value;
+	var fr2 = document.getElementById("fraction2").value;
+	var tmp = document.getElementById("tempo").value;
+	
+	var track = new Track(fr1, fr2, tmp, 1, newLine);
+	track.setLineValue();
+	trackArray.push(track);
+	currentTrack = track;
+}
+
+function createTrackLine() {
+	var trackList = document.getElementById("trackList");
 	var newTrack = document.createElement("div");
 	newTrack.setAttribute("name", "track");
 	var newDescriptor = document.createElement("input");
@@ -43,8 +96,6 @@ function addTrack() {
 	newRepeatCount.setAttribute("max", "1000");
 	newRepeatCount.setAttribute("value", "1");
 	newTrack.appendChild(newRepeatCount);
-	newTrackLi.appendChild(newTrack);
-	trackList.appendChild(newTrackLi);
-	setTrackData();
+	return newTrack;
 }
 
