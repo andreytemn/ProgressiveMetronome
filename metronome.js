8a5e73a981641929d8832c72d2453477b55bfe14
Andrey Temnikov <andreytemn@mail.ru>
2017-07-06 00:08:27 +0500

    Added remove line button

--- metronome.js
+++ metronome.js
@@ -15,11 +15,11 @@ function Track(fraction1, fraction2, tempo, count, line) {
 	}
 }
 
-function getTrackById(id)
+function getTrackByLine(line)
 {
 	for (var track in trackArray)
 	{
-		if (track.line == id)
+		if (track.line == line)
 		{
 			return track;
 		}
@@ -32,16 +32,16 @@ function init() {
 	addTrack();
 	document.getElementById("addNewTrack").onclick = addTrack;
 	document.getElementById("fraction1").onchange = function(){
-		currentLine.fraction1 = document.getElementById("fraction1").value;
-		currentLine.setLineValue();
+		currentTrack.fraction1 = document.getElementById("fraction1").value;
+		currentTrack.setLineValue();
 	};
 	document.getElementById("fraction2").onchange = function(){
-		currentLine.fraction2 = document.getElementById("fraction2").value;
-		currentLine.setLineValue();
+		currentTrack.fraction2 = document.getElementById("fraction2").value;
+		currentTrack.setLineValue();
 	};
 	document.getElementById("tempo").onchange = function(){
-		currentLine.tempo = document.getElementById("tempo").value;
-		currentLine.setLineValue();
+		currentTrack.tempo = document.getElementById("tempo").value;
+		currentTrack.setLineValue();
 	};
 }
 
@@ -54,12 +54,15 @@ function switchLamp() {
 }
 
 function addTrack() {
-	
 	var trackList = document.getElementById("trackList");
 	var newLine = createTrackLine();
-	newLine.onclick = function()
+	newLine.getElementsByTagName("input")[0].onclick = function()
 	{
-		chooseLine(newLine, track);
+		chooseLine(track);
+	}
+	newLine.getElementsByTagName("input")[1].onclick = function()
+	{
+		chooseLine(track);
 	}
 
 	trackList.appendChild(newLine);
@@ -71,22 +74,20 @@ function addTrack() {
 	var track = new Track(fr1, fr2, tmp, 1, newLine);
 	track.setLineValue();
 	trackArray.push(track);
-	currentTrack = track;
-	chooseLine(newLine, track);
+	chooseLine(track);
 }
 
-function chooseLine(newLine, track)
+function chooseLine(track)
 {
-	if ("undefined" !== typeof currentLine) 
+	if ("undefined" !== typeof currentTrack) 
 	{
-		currentLine.line.classList.remove("choosenLine");	
+		currentTrack.line.classList.remove("choosenLine");	
 	}
-	newLine.classList.add("choosenLine");
-	currentLine = track;
+	track.line.classList.add("choosenLine");
+	currentTrack = track;
 }
 
 function createTrackLine() {
-	var trackList = document.getElementById("trackList");
 	var newTrack = document.createElement("div");
 	newTrack.setAttribute("name", "track");
 	var newDescriptor = document.createElement("input");
@@ -101,6 +102,37 @@ function createTrackLine() {
 	newRepeatCount.setAttribute("max", "1000");
 	newRepeatCount.setAttribute("value", "1");
 	newTrack.appendChild(newRepeatCount);
+	var removeLine = document.createElement("button");
+	removeLine.setAttribute("name", "removeButton");
+	removeLine.innerHTML = "-";
+	removeLine.onclick = function()
+	{
+		var trackObj = getTrackByLine(newTrack);
+		for (var i = 0; i < trackArray.length; i++)
+		{
+			if (trackArray[i].line == newTrack)
+			{
+				if (("undefined" !== typeof currentTrack) && (currentTrack === newTrack)) 
+				{
+					if (i > 0)
+					{
+						var newChoosenTrack = trackArray[i - 1];
+						chooseLine(newChoosenTrack);
+					} else if (i < trackArray.length - 1)
+					{
+						var newChoosenTrack = trackArray[i + 1];
+						chooseLine(newChoosenTrack);
+					}
+				}				
+				trackArray.splice(i, 1);
+				break;
+			}
+		}
+		
+		var trackList = document.getElementById("trackList");
+		trackList.removeChild(newTrack);
+	}
+	newTrack.appendChild(removeLine);
 	return newTrack;
 }
 
